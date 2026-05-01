03538507545cf58c9537e4fb901f631f2ef34e2d
Thomas Couzon <thomascouzon@MacBook-Air-de-Thomas.local>
2026-05-01 13:04:58 +0200

    Test resend production emails

--- app/api/reservation/route.ts
+++ app/api/reservation/route.ts
@@ -5,7 +5,7 @@ import { supabaseAdmin } from "@/lib/supabase-admin";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 const CONTACT_PHONE = "02 41 93 39 00";
-const MAX_ONLINE = 10;
+const MAX_ONLINE = 12;
 
 function getDayOfWeek(dateString: string) {
   return new Date(dateString).getDay();
@@ -212,38 +212,37 @@ export async function POST(request: Request) {
     // =========================
     // 📧 EMAIL CLIENT
     // =========================
-    //await resend.emails.send({
-    //  from: "Guinguette <onboarding@resend.dev>", // temporaire
-    //  from: "Guinguette <onboarding@resend.dev>", // temporaire
-    //  to: email,
-    //  subject: "Confirmation de votre réservation 🍷",
-    //  html: `
-    //    <h2>Votre réservation est bien enregistrée</h2>
+    await resend.emails.send({
+      from: "La Guinguette du Père Chapuis <reservation@guinguetteduperechapuis.fr>", 
+      to: email,
+      subject: "Guinguette du Père Chapuis - Confirmation de votre réservation 🍷",
+      html: `
+        <h2>Votre réservation est bien enregistrée</h2>
 
-    //    <p>Bonjour ${nom},</p>
+        <p>Bonjour ${nom},</p>
 
-    //    <p>Nous avons bien reçu votre demande de réservation :</p>
+        <p>Nous avons bien reçu votre demande de réservation :</p>
 
-    //    <ul>
-    //      <li><strong>Date :</strong> ${date}</li>
-    //      <li><strong>Service :</strong> ${normalizedService}</li>
-    //      <li><strong>Heure :</strong> ${heure}</li>
-    //      <li><strong>Nombre de personnes :</strong> ${personnes}</li>
-    //    </ul>
+        <ul>
+          <li><strong>Date :</strong> ${date}</li>
+          <li><strong>Service :</strong> ${normalizedService}</li>
+          <li><strong>Heure :</strong> ${heure}</li>
+          <li><strong>Nombre de personnes :</strong> ${personnes}</li>
+        </ul>
 
-    //    ${
-    //      commentaire
-    //        ? `<p><strong>Commentaire :</strong> ${commentaire}</p>`
-    //         : ""
-    //    }
+        ${
+          commentaire
+            ? `<p><strong>Commentaire :</strong> ${commentaire}</p>`
+             : ""
+        }
 
-    //    <p>🍷 Nous avons hâte de vous accueillir à la guinguette !</p>
+        <p>🍷 Nous avons hâte de vous accueillir à la guinguette !</p>
 
-    //    <p>
-    //      Pour toute modification, merci de nous appeler au ${CONTACT_PHONE}.
-    //    </p>
-    //  `,
-    //});
+        <p>
+          Pour toute modification, merci de nous appeler au ${CONTACT_PHONE}.
+        </p>
+      `,
+    });
     
     // =========================
     // 📧 EMAIL INTERNE
